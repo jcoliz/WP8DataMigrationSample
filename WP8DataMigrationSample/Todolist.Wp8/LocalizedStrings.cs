cbf5e20114f69a035e41b29cb9e333a773cbeba8
James Coliz <jcoliz@xbox.com>
2017-03-10 16:33:00 -0800

    Created a new WP8 app

---
+++ WP8DataMigrationSample/Todolist.Wp8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Todolist.Wp8.Resources;
+
+namespace Todolist.Wp8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
